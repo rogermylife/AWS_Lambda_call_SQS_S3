e7896e69d5989d3f7614d08ade65aaec258eb1bf
Rogermylife <rogermylifeQ@gmail.com>
2017-05-21 14:30:17 +0800

    Ver 1.1 new localhost

--- lambda_image_processor.py
+++ lambda_image_processor.py
@@ -6,10 +6,13 @@ import urllib
 
 
 SQS = boto3.resource('sqs')
-INPUTQ = SQS.Queue('https://sqs.us-east-1.amazonaws.com/005368682193/input')
-OUTPUTQ = SQS.Queue('https://sqs.us-east-1.amazonaws.com/005368682193/output')
+input_queue_url = os.environ['input_queue_url']
+out_queue_url = os.environ['out_queue_url']
+INPUTQ = SQS
+OUTPUTQ = SQS
 
-BUCKET_NAME = 'nthu105062548'
+BUCKET_NAME = os.environ['bucket_name']
+BUCKET_FOLDER = os.environ['bucket_folder']
 S3 = boto3.resource('s3')
 
 
@@ -18,6 +21,8 @@ S3 = boto3.resource('s3')
 # print(response.get('MD5OfMessageBody'))
 
 def lambda_handler(event, context):
+    INPUTQ = SQS.Queue(input_queue_url)
+    OUTPUTQ = SQS.Queue(out_queue_url)  
     for message in INPUTQ.receive_messages():
         # Get the custom author message attribute if it was set
         # Print out the body and author (if set)
@@ -37,25 +42,26 @@ def lambda_handler(event, context):
 def process_message(message, job_id):
     print "process start"
     try:
-        output_dir = "/tmp/"
+        output_dir = "/tmp/%s/" % (job_id)
+        os.makedirs(output_dir)
 		# Download images from URLs specified in message
         for line in message.splitlines():
             print "Downloading image from %s" % line
             # res = os.system("wget -P %s %s" % (output_dir, line))
-            urllib.urlretrieve(line, '/tmp/'+str(uuid.uuid4())+'.jpg')
+            urllib.urlretrieve(line, output_dir+str(uuid.uuid4())+'.jpg')
             # os.system("wget -OutFile %s.jpg %s" % (output_dir, line))
         output_image_name = "output-%s.jpg" % (job_id)
         output_image_path = output_dir + output_image_name
-        listdir()
+        listdir(output_dir)
 		# Invoke ImageMagick to create a montage
         print 'montage '+output_image_name
         os.system("montage -size 400x400 null: %s*.* null: -thumbnail 400x400 -bordercolor white -background black +polaroid -resize 80%% -gravity center -background black -geometry -10+2  -tile x1 %s" % (output_dir, output_image_path))
         print 'montage done'
-        listdir()
+        listdir(output_dir)
         print 'upload file '+output_image_path
-        S3.Object(BUCKET_NAME, output_image_name).put(Body=open(output_image_path, 'rb'))
+        S3.Object(BUCKET_NAME, BUCKET_FOLDER+'/'+output_image_name).put(Body=open(output_image_path, 'rb'))
         print 'upload done'
-        download_url = "https://s3.amazonaws.com/%s/%s" % (BUCKET_NAME, output_image_name)
+        download_url = "https://s3.amazonaws.com/%s/%s/%s" % (BUCKET_NAME, BUCKET_FOLDER, output_image_name)
         return download_url
 
         # Write the resulting image to s3
@@ -66,9 +72,9 @@ def process_message(message, job_id):
     except:
         print "ERRORRRRRRRRRRRRRRRR"
 
-def listdir():
-    print 'show /tmp/'
-    for filename in os.listdir("/tmp/"):
+def listdir(output_dir):
+    print 'show %s' %(output_dir)
+    for filename in os.listdir(output_dir):
         print  filename
 
 def insert_data():
@@ -78,10 +84,3 @@ def insert_data():
                     "https://us-east-1-aws-training.s3.amazonaws.com/arch-static-assets/static/20120728-DSC01315-L.jpg\n"
                     "https://us-east-1-aws-training.s3.amazonaws.com/arch-static-assets/static/20120728-DSC01337-L.jpg\n")
     INPUTQ.send_message(MessageBody=input_messege)
-
-
-
-if __name__ == '__main__':
-    insert_data()
-    lambda_handler("XD", "XD")
-
